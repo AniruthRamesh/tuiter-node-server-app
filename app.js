95447e0782972d66b4ee0a9aa1d5f0267f509450
AniruthRamesh <anikrishna3@gmail.com>
2023-03-29 02:40:31 -0400

    a9 sp23 changes

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ import mongoose from "mongoose"
 const app = express()
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: 'https://a9--hilarious-kitten-f5a246.netlify.app'
+}));
 
 helloController(app)
 TuitsController(app);
